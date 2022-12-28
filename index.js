f564863932dad0cf16e6fb6bf4df415d7f85d1c8
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2022-12-27 18:41:07 -0800

    screenshot

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const puppeteer = require('puppeteer');
 
+async function run() {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://www.traversymedia.com');
+
+    await page.screenshot({ path: 'example.png' });
+    
+    await browser.close();
+}
+
+run();
